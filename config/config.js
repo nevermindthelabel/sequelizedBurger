3e4233a84314ff0f2bd125ac0ff57a5814474c1c
Matt Kilcup <nevermindthelabel@gmail.com>
2019-06-27 19:11:02 -0700

    updated .env config password

--- config/config.js
+++ config/config.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 module.exports = {
   development: {
     username: 'root',
-    password: process.env.DATABASE_PASSWORD,
+    password: process.env.PASS,
     database: 'burgers_db',
     host: '127.0.0.1',
     dialect: 'mysql',
